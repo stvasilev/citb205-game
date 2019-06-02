c455559ed2a7a29c462613e4e92f7ba8560cfb9a
Kiril Vuchkov <kirilvuchkov@gmail.com>
2019-06-02 11:45:05 +0300

    Use sq pixels

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
 class Graphics
 {
 public:
-    Graphics(HANDLE hNewScreenBuffer, int rows, int cols) : rows(rows), cols(cols), hNewScreenBuffer(hNewScreenBuffer)
+    Graphics(HANDLE hNewScreenBuffer, int logicalRows, int logicalCols) : hNewScreenBuffer(hNewScreenBuffer)
     {
+        rows = logicalRows;
+        cols = logicalCols * 2;
         buffer = new CHAR_INFO[rows * cols]{};
-        coordBufSize.Y = this->rows;
+        coordBufSize.Y = rows;
         coordBufSize.X = cols;
 
         coordBufCoord.X = 0;
@@ -16,13 +18,13 @@ public:
 
         srctWriteRect.Top = 0;
         srctWriteRect.Left = 0;
-        srctWriteRect.Bottom = this->rows - 1;
+        srctWriteRect.Bottom = rows - 1;
         srctWriteRect.Right = cols - 1;
     }
     void fillPixel(int x, int y)
     {
-        buffer[y * cols + x].Char.AsciiChar = ' ';
-        buffer[y * cols + x].Attributes = BACKGROUND_GREEN;
+        buffer[y * cols + 2*x].Char.AsciiChar = buffer[y * cols + 2*x+1].Char.AsciiChar = ' ';
+        buffer[y * cols + 2*x].Attributes = buffer[y * cols + 2*x+1].Attributes = BACKGROUND_GREEN;
     }
 
     void fillRect(int topLeftX, int topLeftY, int width, int height)
@@ -89,7 +91,8 @@ int main(void)
     Graphics g(hNewScreenBuffer, 40, 60);
     
     // g.fillPixel(5, 3);
-    g.fillRect(5, 3, 7, 4);
+    // g.fillRect(5, 3, 7, 4);
+    g.fillCircle(10, 10, 5);
 
 
     fSuccess = g.render();
