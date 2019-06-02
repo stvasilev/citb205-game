3ca0d0f73d85a87dba2d00a63982462b711c342f
Kiril Vuchkov <kirilvuchkov@gmail.com>
2019-06-02 11:40:15 +0300

    Refactor with graphics class

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,111 @@
-#include <windows.h> 
+#include <windows.h>
 #include <stdio.h>
- 
-int main(void) 
-{ 
-    HANDLE hStdout, hNewScreenBuffer; 
-    SMALL_RECT srctReadRect; 
-    SMALL_RECT srctWriteRect; 
-    CHAR_INFO chiBuffer[20*80] = {}; // [2][80]; 
-    COORD coordBufSize; 
-    COORD coordBufCoord; 
-    BOOL fSuccess; 
- 
-    // Get a handle to the STDOUT screen buffer to copy from and 
-    // create a new screen buffer to copy to. 
- 
-    hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
-    hNewScreenBuffer = CreateConsoleScreenBuffer( 
-       GENERIC_READ |           // read/write access 
-       GENERIC_WRITE, 
-       FILE_SHARE_READ | 
-       FILE_SHARE_WRITE,        // shared 
-       NULL,                    // default security attributes 
-       CONSOLE_TEXTMODE_BUFFER, // must be TEXTMODE 
-       NULL);                   // reserved; must be NULL 
-    if (hStdout == INVALID_HANDLE_VALUE || 
-            hNewScreenBuffer == INVALID_HANDLE_VALUE) 
-    {
-        printf("CreateConsoleScreenBuffer failed - (%d)\n", GetLastError()); 
-        return 1;
-    }
- 
-    // Make the new screen buffer the active screen buffer. 
- 
-    if (! SetConsoleActiveScreenBuffer(hNewScreenBuffer) ) 
-    {
-        printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError()); 
-        return 1;
-    }
- 
-    // The temporary buffer size is 2 rows x 80 columns. 
- 
-    coordBufSize.Y = 20; 
-    coordBufSize.X = 80; 
- 
-    // The top left destination cell of the temporary buffer is 
-    // row 0, col 0. 
- 
-    coordBufCoord.X = 0; 
-    coordBufCoord.Y = 0; 
- 
-    // Set the destination rectangle. 
- 
-    srctWriteRect.Top = 0;    // top lt: row 10, col 0 
-    srctWriteRect.Left = 0; 
-    srctWriteRect.Bottom = 20; // bot. rt: row 11, col 79 
-    srctWriteRect.Right = 79; 
+#include <iostream>
 
-    for (int i=0; i<20*80; i++) {
-        chiBuffer[i].Char.AsciiChar = ' ';
-        chiBuffer[i].Attributes = BACKGROUND_GREEN;
-    }
- 
-    fSuccess = WriteConsoleOutput( 
-        hNewScreenBuffer, // screen buffer to write to 
-        chiBuffer,        // buffer to copy from 
-        coordBufSize,     // col-row size of chiBuffer 
-        coordBufCoord,    // top left src cell in chiBuffer 
-        &srctWriteRect);  // dest. screen buffer rectangle 
-    if (! fSuccess) 
+class Graphics
+{
+public:
+    Graphics(HANDLE hNewScreenBuffer, int rows, int cols) : rows(rows), cols(cols), hNewScreenBuffer(hNewScreenBuffer)
     {
-        printf("WriteConsoleOutput failed - (%d)\n", GetLastError()); 
+        buffer = new CHAR_INFO[rows * cols]{};
+        coordBufSize.Y = this->rows;
+        coordBufSize.X = cols;
+
+        coordBufCoord.X = 0;
+        coordBufCoord.Y = 0;
+
+        srctWriteRect.Top = 0;
+        srctWriteRect.Left = 0;
+        srctWriteRect.Bottom = this->rows - 1;
+        srctWriteRect.Right = cols - 1;
+    }
+    void fillPixel(int x, int y)
+    {
+        buffer[y * cols + x].Char.AsciiChar = ' ';
+        buffer[y * cols + x].Attributes = BACKGROUND_GREEN;
+    }
+
+    void fillRect(int topLeftX, int topLeftY, int width, int height)
+    {
+        for (int x = topLeftX; x < topLeftX+width; x++) {
+            for (int y = topLeftY; y < topLeftY + height; y++) {
+                fillPixel(x, y);
+            }
+        }
+    }
+
+    bool render()
+    {
+        return WriteConsoleOutput(
+            hNewScreenBuffer, // screen buffer to write to
+            buffer,           // buffer to copy from
+            coordBufSize,     // col-row size of chiBuffer
+            coordBufCoord,    // top left src cell in chiBuffer
+            &srctWriteRect);  // dest. screen buffer rectangle
+    }
+
+private:
+    HANDLE hNewScreenBuffer;
+    CHAR_INFO *buffer;
+    COORD coordBufSize;
+    COORD coordBufCoord;
+    SMALL_RECT srctWriteRect;
+
+    int rows;
+    int cols;
+};
+int main(void)
+{
+    HANDLE hStdout, hNewScreenBuffer;
+    BOOL fSuccess;
+
+    // Get a handle to the STDOUT screen buffer to copy from and
+    // create a new screen buffer to copy to.
+
+    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    hNewScreenBuffer = CreateConsoleScreenBuffer(
+        GENERIC_READ | // read/write access
+            GENERIC_WRITE,
+        FILE_SHARE_READ |
+            FILE_SHARE_WRITE,    // shared
+        NULL,                    // default security attributes
+        CONSOLE_TEXTMODE_BUFFER, // must be TEXTMODE
+        NULL);                   // reserved; must be NULL
+    if (hStdout == INVALID_HANDLE_VALUE ||
+        hNewScreenBuffer == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateConsoleScreenBuffer failed - (%d)\n", GetLastError());
         return 1;
     }
-    Sleep(5000);
- 
-    // Restore the original active screen buffer. 
- 
-    if (! SetConsoleActiveScreenBuffer(hStdout)) 
+
+    // Make the new screen buffer the active screen buffer.
+
+    if (!SetConsoleActiveScreenBuffer(hNewScreenBuffer))
     {
-        printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError()); 
+        printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
+        return 1;
+    }
+
+    Graphics g(hNewScreenBuffer, 40, 60);
+    
+    // g.fillPixel(5, 3);
+    g.fillRect(5, 3, 7, 4);
+
+
+    fSuccess = g.render();
+    if (!fSuccess)
+    {
+        printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
+        return 1;
+    }
+
+    Sleep(5000);
+
+    // Restore the original active screen buffer.
+
+    if (!SetConsoleActiveScreenBuffer(hStdout))
+    {
+        printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
         return 1;
     }
 
